45cfead93f9362039a5315a27287373c5e75b88e
Portafolio FullStack Developer de ArturoTexada <arturo.tejada@gmail.com>
2022-11-17 01:59:38 -0600

    first commit

---
+++ routes/usuarios.js
@@ -0,0 +1,30 @@
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { usuariosGet,
+        usuariosPut,
+        usuariosPost,
+        usuariosDelete,
+        usuariosPatch } = require('../controllers/usuarios');
+
+const router = Router();
+
+
+router.get('/', usuariosGet );
+
+router.put('/:id', usuariosPut );
+
+router.post('/', 
+[ check('correo', 'El correo no es válido').isEmail() ] 
+               , usuariosPost );
+
+router.delete('/', usuariosDelete );
+
+router.patch('/', usuariosPatch );
+
+
+
+
+
+module.exports = router;
